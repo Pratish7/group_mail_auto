3b0b957ba495a9a716636b008529c87249676243
Pratish7 <pratishbajpai7@gmail.com>
2020-12-14 16:14:19 +0530

    added treeview

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from tkinter import Tk, Frame, Label, Entry, GROOVE, Text, Button
+from tkinter import Tk, Frame, Label, Entry, GROOVE, Text, Button, ttk, CENTER
 
 root = Tk()
 
-login_frame = Frame(root, relief=GROOVE, border=1)      #login frame
+main_frame_0 = Frame(root)
+
+login_frame = Frame(main_frame_0, relief=GROOVE, border=1)      #login frame
 
 email_label = Label(login_frame, text='email')
 email_label.grid(row=0, column=0, sticky='W')
@@ -19,13 +21,13 @@ pass_entry.grid(row=1, column=1)
 login_frame.grid(row=0, column=0)
 
 
-body_frame = Frame(root, relief=GROOVE, border=1)        #body frame
+body_frame = Frame(main_frame_0, relief=GROOVE, border=1)        #body frame
 
 subject_label = Label(body_frame, text='Subject')
 subject_label.grid(row=0, column=0, sticky='N')
 
 subject_entry = Entry(body_frame)
-subject_entry.grid(row=0, column=1)
+subject_entry.grid(row=0, column=1, sticky='nesw')
 
 mail_label = Label(body_frame, text='Mail body')
 mail_label.grid(row=1, column=0)
@@ -35,7 +37,7 @@ mail_entry.grid(row=1, column=1)
 
 body_frame.grid(row=1, column=0)
 
-btn_frame = Frame(root)                                #button frame
+btn_frame = Frame(main_frame_0)                                #button frame
 
 btn_send = Button(btn_frame, text='Send')
 btn_send.grid(row=0, column=0, columnspan=3, sticky='nesw')
@@ -50,5 +52,19 @@ btn_rest_all = Button(btn_frame, text='Reset All')
 btn_rest_all.grid(row=1, column=2)
 
 btn_frame.grid(row=2, column=0)
+
+main_frame_0.grid(row=0, column=0)
+
+
+mailers_frame = Frame(root)
+
+table = ttk.Treeview(mailers_frame)
+table['show'] = 'headings'
+table["columns"] = ("Mail", "Name")
+table.column("Mail", stretch=False, width=90, anchor=CENTER)
+table.column("Name", stretch=False, width=150, anchor=CENTER)
+table.grid(column=0, row=0, rowspan=1)
+
+mailers_frame.grid(row=0, column=1, sticky='nsew')
 
 root.mainloop()
